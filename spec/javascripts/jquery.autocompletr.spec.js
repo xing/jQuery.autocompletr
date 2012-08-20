ce210f56f60686fb38a2c18d01a9764a43c39e90
Calamari <g.tavonius@gmail.com>
2012-08-20 15:37:40 +0200

    Add Aray.prototype.filter shim in specs for passing IE<=8

--- spec/javascripts/jquery.autocompletr.spec.js
+++ spec/javascripts/jquery.autocompletr.spec.js
@@ -1,3 +1,32 @@
+// add shim for IE7/8
+// borrowed from: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/filter
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun /*, thisp */) {
+    if (this == null) {
+      throw new TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun != "function") {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}
+
 describe("jQuery.autocompletr", function() {
   var input, input2;
   var ajaxCount, ajaxOptions, ajaxDone;
